7555226e3388f04bac02d933c24fc7dd513439e3
Alex Bennee <ajb@pitcairn.cambridgebroadband.com>
2009-09-04 13:41:54 +0100

    Refactor for cleanliness

--- dbgsym.py
+++ dbgsym.py
@@ -12,29 +12,36 @@ import string
 
 debug_path="/usr/lib/debug"
 
-def check_symbol_file(file):
+def compare_crcs(debug_file, actual_file):
+    # Calculate the CRC of the debug file
+    data = open(debug_file).read()
+    debug_crc = binascii.crc32(data)
+    debug_crc = debug_crc % 2**32  # force to be signed
+
+    # Extract the CRC from the actual file
+    obj_output = commands.getoutput("objdump -s -j .gnu_debuglink "+actual_file)
+
+    # The last 8 hex digits are the (byte reversed) CRC
+    bytes = re.findall("[0-9a-f]{8}", obj_output)
+    obj_crc = bytes[len(bytes)-1] # I'm sure there is a more concise way
+    swap_crc=[]
+    for i in range(0, len(obj_crc), 2):
+        swap_crc.insert(0, obj_crc[i:i+2])
+    real_crc = string.atol(''.join(swap_crc), 16)
+
+    # Finally complain if the CRCs do not match
+    if real_crc != debug_crc:
+        print "debug_file: %s has CRC of %X" % (debug_file, debug_crc)
+        print "actual_file: %s thinks CRC should be %X" % (actual_file, real_crc)
+    
+
+def process_debug_file(debug_file):
     # We now need to calculate the path the debug library is refering to
-    target_file = re.sub(debug_path, "", file)
+    target_file = re.sub(debug_path, "", debug_file)
     if os.path.isfile(target_file):
-        # OK, we have a match. Calculate the CRC of the debug library
-        data = open(file).read()
-        crc = binascii.crc32(data)
-        crc = crc % 2**32  
-
-        # Now find out what the parent file thinks the CRC should be
-        obj_output = commands.getoutput("objdump -s -j .gnu_debuglink "+target_file)
-        # The last 8 hex digits are the (byte reversed) CRC
-        bytes = re.findall("[0-9a-f]{8}", obj_output)
-        obj_crc = bytes[len(bytes)-1] # I'm sure there is a more concise way
-        swap_crc=[]
-        for i in range(0, len(obj_crc), 2):
-            swap_crc.insert(0, obj_crc[i:i+2])
-        real_crc = string.atol(''.join(swap_crc), 16)
-
-        # Finally complain if the CRCs do not match
-        if real_crc != crc:
-            print "debug_file: %s has CRC of %X" % (file, crc)
-            print "library_file: %s thinks CRC is %X" % (target_file, real_crc)
+        compare_crcs(debug_file, target_file)
+    else:
+        print "couldn't find paired library for: %s" % (debug_file)
 
 def check_dir(dir):
     for e in os.listdir(dir):
@@ -42,8 +49,7 @@ def check_dir(dir):
         if os.path.isdir(path):
             check_dir(path)
         else:
-            print "checking: %s" % (path)
-            check_symbol_file(path)
+            process_debug_file(path)
     
 
 check_dir(debug_path)
